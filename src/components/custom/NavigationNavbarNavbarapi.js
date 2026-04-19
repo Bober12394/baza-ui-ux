4d7d330024ffee8530ac899bc6f0ae1694d6f4e6
Bober12394 <79087509+Bober12394@users.noreply.github.com>
2026-04-19 18:29:39 +0200

    Collapse navbar on outside click and Home action

---
+++ src/components/custom/NavigationNavbarNavbarapi.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NavigationNavbarNavbarapi() {
+  return (
+    <section className="generated-component" aria-label="NavigationNavbarNavbarapi">
+      <h3>NavigationNavbarNavbarapi</h3>
+      <p>To jest komponent custom załadowany z pliku `NavigationNavbarNavbarapi.js`.</p>
+    </section>
+  );
+}
+
+export default NavigationNavbarNavbarapi;
